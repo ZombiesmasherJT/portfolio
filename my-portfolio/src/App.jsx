201407beb061cffbd28f8f37f8513781c9358fa6
joshua <Joshua.tite@me.com>
2025-05-29 17:39:56 +0100

    changed teck stack for portfolio

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="section">
+        <h1>new tech stack update</h1>
+        <p>Transfroming my digital portfolio</p>
+      </main>
+    </>
+  );
+}
+
+export default App
---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">JGT</div>
+            <ul className="nav-links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
