2cccdba349cc0c5034ad887b6930e00d8a66786b
joshua <Joshua.tite@me.com>
2025-06-30 18:42:40 +0100

    contact functionality working with formspree and styled with small annimation.

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -9,3 +9,36 @@ function ContactForm() {
     const [state, handleSubmit] = useForm("mrbkjlod");
 
 
+    return (
+        <section id="contact">
+            <div className="contact-container">
+                <h2 className="contact-heading">Let's Connect</h2>
+                {state.succeeded ? (
+                    <div className="success-message">
+                        <h3>Thanks for reaching out!</h3>
+                        <p>I’ll be in touch with you shortly 👋</p>
+                    </div>
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="name">Name</label>
+                        <input id="name" type="text" name="name" required />
+
+                        <label htmlFor="email">Email Address</label>
+                        <input id="email" type="email" name="email" required />
+                        <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" name="message" required />
+                        <ValidationError prefix="Message" field="message" errors={state.errors} />
+
+                        <button type="submit" disabled={state.submitting}>
+                            {state.submitting ? "Sending..." : "Submit"}
+                        </button>
+                    </form>
+                )}
+            </div>
+        </section>
+    );
+}
+
+export default ContactForm;
